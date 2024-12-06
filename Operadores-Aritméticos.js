0710c6659c7576a28f5be8672229b18218b4da9b
walterdyna <alexdyna@gmail.com>
2024-12-06 11:30:21 -0300

    first commit

---
+++ Operadores-Aritméticos.js
@@ -0,0 +1,46 @@
+/*
+
+Operadores Aritméticos
+
++ -> Adição
+- -> Subtração
+/ -> Divisão
+* -> Multiplicação
+% -> Resto
+++ -> Incremento
+-- -> Decremento
+** -> Exponencial
+
+*/
+
+//resultados com operadores aritméticos
+
+const result = 10 + 5;
+console.log(result); // 15
+
+const result2 = 10 - 5;
+console.log(result2); // 5
+
+const result3 = 10 / 5;
+console.log(result3) // 2
+
+const result4 = 10 * 5;
+console.log(result4) // 50
+
+const result5 = 10 % 5;
+console.log(result5) // 0
+
+let result6 = 10;
+result6++;//incremento
+console.log(result6) // 11
+
+const result7 = 10;
+result7--;//decremento
+console.log(result7) // 9
+
+const result8 = 10 ** 5;
+console.log(result8) // 100000
+
+const result9 = 10 ^ 3;
+console.log(result9) // 1000
+
